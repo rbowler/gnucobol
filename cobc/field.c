88d556e1074094f3e488747245bccaae3e74855d
Roger Bowler <rbowler@snipix.net>
2025-10-13 15:29:08 +0200

    Fix binary size computation for COMP-5 and BINARY-CHAR

--- cobc/field.c
+++ cobc/field.c
@@ -2227,6 +2227,7 @@ validate_elementary_item (struct cb_field *f)
 		f->usage = CB_USAGE_COMP_5;
 		f->pic = cb_build_binary_picture ("BINARY-CHAR", 2, 1);
 		f->flag_real_binary = 1;
+		f->flag_binary_char = 1;
 		break;
 	case CB_USAGE_SIGNED_SHORT:
 		f->usage = CB_USAGE_COMP_5;
@@ -2247,6 +2248,7 @@ validate_elementary_item (struct cb_field *f)
 		f->usage = CB_USAGE_COMP_5;
 		f->pic = cb_build_binary_picture ("BINARY-CHAR", 2, 0);
 		f->flag_real_binary = 1;
+		f->flag_binary_char = 1;
 		break;
 	case CB_USAGE_UNSIGNED_SHORT:
 		f->usage = CB_USAGE_COMP_5;
@@ -2267,6 +2269,9 @@ validate_elementary_item (struct cb_field *f)
 	case CB_USAGE_UNSIGNED_LONG:
 		f->usage = CB_USAGE_COMP_5;
 		f->pic = cb_build_binary_picture ("BINARY-DOUBLE", 18, 0);
+		f->flag_real_binary = 1;
+		break;
+	case CB_USAGE_COMP_5:
 		f->flag_real_binary = 1;
 		break;
 	default:
@@ -2513,6 +2518,8 @@ setup_parameters (struct cb_field *f)
 		break;
 
 	case CB_USAGE_COMP_5:
+		f->flag_real_binary = 1;
+		/* Fall-through */
 	case CB_USAGE_COMP_X:
 	case CB_USAGE_COMP_N:
 		if (f->pic->category == CB_CATEGORY_ALPHANUMERIC) {
@@ -2556,7 +2563,7 @@ compute_binary_size (struct cb_field *f, const int size)
 			   (size <= 9) ? 4 : (size <= 18) ? 8 : 16);
 		return;
 	case CB_BINARY_SIZE_2_4_8:
-		if (f->flag_real_binary && size <= 2) {
+		if (f->flag_binary_char) {
 			f->size = 1;
 		} else {
 			f->size = ((size <= 4) ? 2 :
