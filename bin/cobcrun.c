40d086a2a442620b7ee547cb4e5c25a9c48bf32d
Roger Bowler <rbowler@snipix.net>
2025-11-13 20:54:03 +0100

    cobcrun --exec-parm option

--- bin/cobcrun.c
+++ bin/cobcrun.c
@@ -39,8 +39,16 @@
 static int arg_shift = 1;
 static int print_runtime_wanted = 0;
 static signed int	verbose_output = 0;
+#define MAX_EXEC_PARM_TEXT_SIZE 100
+static int exec_parm_wanted = 0;
+static struct {
+	unsigned char length[2];
+	unsigned char text[MAX_EXEC_PARM_TEXT_SIZE];
+} __attribute__((packed)) exec_parm;
+static const int exec_parm_argc = 1;
+static char *exec_parm_argv[1] = { (char *)&exec_parm };
 
-static const char short_options[] = "+hirc:VvqM:";
+static const char short_options[] = "+hirc:VvqM:p:";
 
 #define	CB_NO_ARG	no_argument
 #define	CB_RQ_ARG	required_argument
@@ -56,6 +64,7 @@ static const struct option long_options[] = {
 	{"runtime-config",		CB_NO_ARG, NULL, 'r'},
 	{"config",		CB_RQ_ARG, NULL, 'C'},
 	{"module",		CB_RQ_ARG, NULL, 'm'},
+	{"exec-parm",		CB_RQ_ARG, NULL, 'p'},
 	{NULL, 0, NULL, 0}
 };
 
@@ -68,6 +77,43 @@ static const struct option long_options[] = {
 #define N_(s)		s
 #endif
 
+/**
+ * Conversion table from ISO 8859-1 (CP819) to EBCDIC codepage 1047
+ */
+static const unsigned char cp819_to_cp1047[256] = {
+  /* 0x00-0x0F */
+  0x00, 0x01, 0x02, 0x03, 0x37, 0x2D, 0x2E, 0x2F, 0x16, 0x05, 0x25, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+  /* 0x10-0x1F */
+  0x10, 0x11, 0x12, 0x13, 0x3C, 0x3D, 0x32, 0x26, 0x18, 0x19, 0x3F, 0x27, 0x1C, 0x1D, 0x1E, 0x1F,
+  /* 0x20-0x2F */
+  0x40, 0x4F, 0x7F, 0x7B, 0x5B, 0x6C, 0x50, 0x7D, 0x4D, 0x5D, 0x5C, 0x4E, 0x6B, 0x60, 0x4B, 0x61,
+  /* 0x30-0x3F */
+  0xF0, 0xF1, 0xF2, 0xF3, 0xF4, 0xF5, 0xF6, 0xF7, 0xF8, 0xF9, 0x7A, 0x5E, 0x4C, 0x7E, 0x6E, 0x6F,
+  /* 0x40-0x4F */
+  0x7C, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xD1, 0xD2, 0xD3, 0xD4, 0xD5, 0xD6,
+  /* 0x50-0x5F */
+  0xD7, 0xD8, 0xD9, 0xE2, 0xE3, 0xE4, 0xE5, 0xE6, 0xE7, 0xE8, 0xE9, 0xAD, 0xE0, 0xBD, 0x5A, 0x6D,
+  /* 0x60-0x6F */
+  0x79, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96,
+  /* 0x70-0x7F */
+  0x97, 0x98, 0x99, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xC0, 0x4A, 0xBB, 0xD0, 0xA1,
+  /* 0x80-0x8F */
+  0x07, 0x20, 0x21, 0x22, 0x23, 0x24, 0x15, 0x06, 0x17, 0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x09, 0x0A,
+  /* 0x90-0x9F */
+  0x1B, 0x30, 0x31, 0x1A, 0x33, 0x34, 0x35, 0x36, 0x08, 0x38, 0x39, 0x3A, 0x3B, 0x04, 0x14, 0x3E,
+  /* 0xA0-0xAF */
+  0xFF, 0x41, 0xAA, 0xB0, 0xB1, 0x9F, 0xB2, 0xD0, 0xA0, 0xB5, 0x79, 0xB4, 0x9A, 0x8A, 0xBA, 0xCA,
+  /* 0xB0-0xBF */
+  0xAF, 0xA8, 0xB7, 0xB8, 0xB9, 0xAB, 0x64, 0x65, 0x62, 0x66, 0x63, 0x67, 0x9E, 0x68, 0x74, 0x71,
+  /* 0xC0-0xCF */
+  0x72, 0x73, 0x78, 0x75, 0x76, 0x77, 0xAC, 0x69, 0xED, 0xEE, 0xEB, 0xEF, 0xEC, 0xBF, 0x80, 0xBC,
+  /* 0xD0-0xDF */
+  0x90, 0x8F, 0xEA, 0xFA, 0xBE, 0xA7, 0x8C, 0x8B, 0xB6, 0xA9, 0xB3, 0xA6, 0xB8, 0xB0, 0xB1, 0xB2,
+  /* 0xE0-0xEF */
+  0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xD1, 0xD2, 0xD3, 0xD4, 0xD5, 0xD6,
+  /* 0xF0-0xFF */
+  0xD7, 0xD8, 0xD9, 0xE2, 0xE3, 0xE4, 0xE5, 0xE6, 0xE7, 0xE8, 0xE9, 0xAD, 0xE0, 0xBD, 0x5A, 0x6D
+};
 
 /**
  * Display cobcrun version info, optional with build and version date
@@ -143,6 +189,10 @@ cobcrun_print_usage (char * prog)
 			"                                  dynamic link loader library search path\n"
 			"                                  and any basename to the module preload list\n"
 			"                                  (COB_LIBRARY_PATH and/or COB_PRELOAD)"));
+	puts (_("  -p \"<parameter>\", --exec-parm=\"<parameter>\"  set parameter string to be\n"
+			"                                  passed to the program in EBCDIC preceded by a 2-byte\n"
+			"                                  big-endian length field. The length does not include\n"
+			"                                  the length of the length field itself."));
 	putchar ('\n');
 #ifndef PACKAGE_BUGREPORT_URL
 	printf (_("Report bugs to: %s\n"
@@ -377,6 +427,24 @@ process_command_line (int argc, char *argv[])
 			}
 			break;
 
+		case 'p':
+			/* -p <parameter>, --exec-parm=<parameter> */
+			exec_parm_wanted = 1;
+			arg_shift++;
+			if (strlen (cob_optarg) > sizeof(exec_parm.text)) {
+				fprintf (stderr, _("exec-parm parameter is too long"));
+				putc ('\n', stderr);
+				fflush (stderr);
+				exit (EXIT_FAILURE);
+			}
+			for (int i = 0; i < strlen (cob_optarg); i++) {
+				exec_parm.text[i] = cp819_to_cp1047[(unsigned char)cob_optarg[i]];
+			}
+			exec_parm.length[0] = (unsigned char)(strlen (cob_optarg) >> 8);
+			exec_parm.length[1] = (unsigned char)(strlen (cob_optarg) & 0xFF);
+			arg_shift++;
+			break;
+
 		/* LCOV_EXCL_START */
 		default:
 			/* not translated as it is an unlikely internal error: */
@@ -425,10 +493,22 @@ main (int argc, char **argv)
 		return 1;
 	}
 
+	/* No other program arguments are allowed if exec-parm is specified */
+	if (exec_parm_wanted && argc > arg_shift + 1) {
+		fprintf (stderr, _("Additional arguments are not allowed when exec-parm is specified"));
+		putc ('\n', stderr);
+		fflush (stderr);
+		return 1;
+	}
+
 	/* Initialize the COBOL system, ... */
 	/* Note: we use cob_init_nomain here as there are no functions
 	         linked here we want to provide for the COBOL environment */
-	cob_init_nomain (argc - arg_shift, &argv[arg_shift]);
+	if (exec_parm_wanted) {
+		cob_init_nomain (exec_parm_argc, exec_parm_argv);
+	} else {
+		cob_init_nomain (argc - arg_shift, &argv[arg_shift]);
+	}
 	if (print_runtime_wanted) {
 		print_runtime_conf ();
 		putc ('\n', stdout);
@@ -443,5 +523,9 @@ main (int argc, char **argv)
 	/* Note:  we requested a program exit if resolving had issues,
 	          so are only still running if we have a a valid, _likely_ COBOL
 	          function to execute */
-	cob_stop_run (unifunc.funcint());
+	if (exec_parm_wanted) {
+		cob_stop_run (unifunc.funcint(&exec_parm));
+	} else {
+		cob_stop_run (unifunc.funcint());
+	}
 }
