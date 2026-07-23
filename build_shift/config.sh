706cb990e7e2ad76f82efbe3c7c14330d3f72d50
Roger Bowler <rbowler@snipix.net>
2026-07-23 22:40:14 +0200

    Propagate gnucobol-shift version number from control file to config.h

--- build_shift/config.sh
+++ build_shift/config.sh
@@ -5,14 +5,19 @@ if [ "${CURDIR##*/}" != "gnucobol" ]; then
   echo "configure should be run from the gnucobol directory"
   exit 1
 fi
+COBVER=$(awk '/Version:/{print$2}' build_shift/control_gnucobol-shift)
 ./autogen.sh
 mkdir -p build
 cd build
 CFLAGS="-g -rdynamic" ../configure \
  --with-pkgversion=gnucobol-shift \
+ --with-bugurl=https://veliashift.com/support \
  --enable-debug \
  --enable-cobc-internal-checks \
  --with-xml2 \
  --with-json \
  --with-curses=no \
  --without-db
+ sed -e "/PACKAGE_VERSION/s/3.3-dev/$COBVER/" \
+  -e "/PACKAGE_BUGREPORT/s/gnu.org/veliashift.com/" \
+  -i config.h
